40b59f16b41401e3d5b83faffd777c78b0802034
Shashank Dangi <91678732+shashankdangi@users.noreply.github.com>
2023-04-11 15:42:01 +0530

    Random Jokes Generator

---
+++ Joke/script.js
@@ -0,0 +1,31 @@
+const jokeContainer = document.getElementById("joke");
+const btn = document.getElementById("btn");
+const url = "https://v2.jokeapi.dev/joke/Any";
+
+let getJoke = () => {
+  fetch(url)
+    .then(data => data.json())
+    .then(item => {
+      if (item.type === "single") {
+        jokeContainer.textContent = `${item.joke}`;
+      } else if (item.type === "twopart") {
+        jokeContainer.textContent = `${item.setup} ${item.delivery}`;
+      } else {
+        jokeContainer.textContent = "Oops, no joke found!";
+      }
+    })
+    .catch(error => {
+      jokeContainer.textContent = "Oops, something went wrong!";
+      console.error(error);
+    });
+}
+
+btn.addEventListener("click", getJoke);
+
+getJoke();
+document.addEventListener("keypress", event => {
+    if (event.key === "Enter") {
+      getJoke();
+    }
+  });
+
